f87cb7e94830a36c27c7fd605df8cacc7adca564
Gercar897 <gerardogardunorubi@hotmail.com>
2021-03-11 11:49:51 -0600

    Navbar styled link
    Navbar basica con styled components y links

--- next-website/components/Navbar.js
+++ next-website/components/Navbar.js
@@ -1,10 +1,34 @@
 import React from 'react'
+import styled from 'styled-components';
+import Link from 'next/link';
+
+const Nav = styled.nav`
+    height: 80px;
+    background: #000;
+    color:#fff;
+`
+    
 
 const Navbar = () => {
     return (
-        <div>
-            <h1>Navbar</h1>
-        </div>
+        <Nav>
+            <div>
+                <Link href="/">
+                    <a>NXT</a>
+                </Link>
+            </div>
+            <div>
+            <Link href="/">
+                    <a>Home</a>
+                </Link>
+                <Link href="/about">
+                    <a>About</a>
+                </Link> 
+                <Link href="/contact">
+                    <a>Contact</a>
+                </Link>
+            </div>
+        </Nav>
     )
 }
 
